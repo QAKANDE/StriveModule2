7a60ab40e2e370d903587b05962f0dbdf0267d43
QAKANDE <58850207+QAKANDE@users.noreply.github.com>
2020-05-13 23:03:52 +0100

    spotify

---
+++ spotify/spotify.js
@@ -0,0 +1,6 @@
+function btnAnimation(){
+    let btns = document.querySelectorAll('.btn')
+    btns.style.border = 'red';
+}
+let btns = document.querySelectorAll('.btn')
+btns.addEventListener('click', btnAnimation)
