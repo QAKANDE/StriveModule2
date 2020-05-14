60bd9f4b589c714d437e7d89a6d361b531b62ff3
QAKANDE <58850207+QAKANDE@users.noreply.github.com>
2020-05-14 14:13:35 +0100

    commit

--- spotify/spotify.js
+++ spotify/spotify.js
@@ -1,6 +1,18 @@
-function btnAnimation(){
-    let btns = document.querySelectorAll('.btn')
-    btns.style.border = 'red';
+let playing = true;
+function playPause(button){
+
+    var x = $('#playPause');
+    $(button).find('i').remove();
+    if ($(button).text().trim() == 'Play') {
+      $(button).html($('<i/>',{class:'fa fa-pause'})).append(' Pause');
+      thumbnail.style.transform = "scale(1.15)"
+      let songdetails = document.querySelectorAll('.songdetails')
+      songdetails.style.color="black";
+      x.fadeIn();
+     }
+     else {
+       $(button).html($('<i/>',{class:'fa fa-play-circle fa-lg'})).append(' Play');
+       thumbnail.style.transform = "scale(1)"
+       x.fadeOut();
+     }
 }
-let btns = document.querySelectorAll('.btn')
-btns.addEventListener('click', btnAnimation)
